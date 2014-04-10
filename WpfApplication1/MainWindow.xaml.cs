56ec4dcf7a62d74a952a69ed89585dbb01902b53
keysharpener <robin.francillon@gmail.com>
2014-04-11 01:47:49 +0200

    Added more complex project structure & added FolderBrowser

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -7,11 +7,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace WpfApplication1
 {
@@ -23,6 +25,13 @@ namespace WpfApplication1
         public MainWindow()
         {
             InitializeComponent();
+        } 
+       
+       private void Button_MouseClick(object sender, RoutedEventArgs e)
+        {
+            var browser = new FolderBrowserDialog();
+            browser.ShowDialog();
+           if (browser.SelectedPath != null) Path.Text= browser.SelectedPath;
         }
     }
 }
